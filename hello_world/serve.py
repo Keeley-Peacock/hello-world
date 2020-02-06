43d658b5fed495b173f6fe0329ed34129dc65a6d
Keeley Peacock <llamagrill@gmail.com>
2020-02-06 14:42:16 +0000

    style: flake8

--- hello_world/serve.py
+++ hello_world/serve.py
@@ -4,12 +4,12 @@ from flask import render_template
 # creates a Flask application, named app
 app = Flask(__name__)
 
-# a route where we will display a welcome message via an HTML template
+
 @app.route("/")
 def hello():
     text = "You smell like fish"
     return render_template('index.html', message=text)
 
-# run the application
+
 if __name__ == "__main__":
     app.run(debug=True)
